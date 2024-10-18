8eb3b799a7cf389479b732c41f0978491e6c3beb
anirudhgv1805 <anirudhgv04@gmail.com>
2024-10-18 07:05:58 +0530

    with this commmit we made the basic setup of the react frontend ,typescript and tailwind css

---
+++ AcademicChimes/src/App.tsx
@@ -0,0 +1,10 @@
+
+import './index.css'
+
+function App(){
+  return(
+    <h1 className= "text-5xl text-center text-green-600 font-bold justify-center items-center h-screen flex"> Successfully setup a react project with typescript and tailwind css</h1>
+  );
+}
+
+export default App
