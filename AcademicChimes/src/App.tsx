de0d26b8b2f8385d8e519924d62ce3a64921a8c6
anirudhgv1805 <anirudhgv04@gmail.com>
2024-10-26 01:09:47 +0530

    Registration page is done

--- AcademicChimes/src/App.tsx
+++ AcademicChimes/src/App.tsx
@@ -1,9 +1,9 @@
-
+import Registration from './Registration.jsx'
 import './index.css'
 
 function App(){
   return(
-    <h1 className= "text-5xl text-center text-green-600 font-bold justify-center items-center h-screen flex"> Successfully setup a react project with typescript and tailwind css</h1>
+    <Registration/>
   );
 }
 
